65bf1e34947e4cd5994c98051a66e520932cede2
Arequipa Laboratorio 01 <Arequipa-Lab01@certus.edu.pe>
2022-09-21 13:45:01 -0500

    comentario

--- index.py
+++ index.py
@@ -1,7 +1,22 @@
-edad=20
-if edad>18:
-    print("Mayor de edad")
-elif edad<=0:
-    print("tu edad no puede ser menor a 0", sep= " /", end="\n" )
-else:
-    "Menor de edad"
+num1 = int(input("introduce tu primer numero"))
+num2 = int(input("introduce tu segundo numero"))
+opcion= 0
+while True:
+    print("""opciones:
+    1) Suma
+    2) Resta
+    3) Salir""")
+    opcion=int(input("Ingresa la operacion a realizar"))
+    if opcion == 1:
+        print("la suma es: ", num1 + num2)
+        print("¿Desea realizar otra operacion? s/n")
+        rpta=input()
+        if rpta=="s":
+            continue
+        else:
+            break
+
+    elif opcion == 2:
+        print("la resta es: ", num1 - num2)
+        print("¿Desea realizar otra operacion? s/n")
+        rpta=input()
